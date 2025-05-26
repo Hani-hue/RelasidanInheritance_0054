b96674a697ad7b0b120e8c213b7cba6728bee0f9
Hani-hue <isnahabibah8729@gmail.com>
2025-05-26 13:27:19 +0700

    buat class pasien

--- Asosiasi/asociation.cpp
+++ Asosiasi/asociation.cpp
@@ -3,3 +3,21 @@
 using namespace std;
 // nama repo :
 // RelasidanInheritance
+
+class dokter;
+class pasien
+{ // asosiasi
+public:
+    string nama;
+    vector<dokter *> daftar_dokter;
+    pasien(string pNama) : nama(pNama)
+    {
+        cout << "pasien \"" << nama << "\" ada \n";
+    }
+    ~pasien()
+    {
+        cout << "pasien \"" << nama << "\" tidak ada \n";
+    }
+    void tambahDokter(dokter *);
+    void cetakDokter();
+};
